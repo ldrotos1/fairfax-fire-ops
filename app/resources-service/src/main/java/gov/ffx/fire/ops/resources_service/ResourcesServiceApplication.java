e90246b38b50e7bff09a171d60f4df48eb538b8a
Louis <louis.drotos@gmail.com>
2025-05-29 20:38:13 -0400

    Added initial resources services project

---
+++ app/resources-service/src/main/java/gov/ffx/fire/ops/resources_service/ResourcesServiceApplication.java
@@ -0,0 +1,13 @@
+package gov.ffx.fire.ops.resources_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourcesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourcesServiceApplication.class, args);
+	}
+
+}
